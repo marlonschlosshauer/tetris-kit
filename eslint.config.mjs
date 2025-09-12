102df54dd42f8acae66dbdf49bc66716daa53610
Marlon Schlosshauer <Marlonschlosshauer@gmail.com>
2025-09-12 20:23:17 +0200

    Allow (some) relative imports

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,19 +6,25 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    plugins: {
-      "@limegrass/import-alias": (await import("@limegrass/eslint-plugin-import-alias")).default,
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        plugins: {
+            "@limegrass/import-alias": (await import("@limegrass/eslint-plugin-import-alias"))
+                .default,
+        },
+        rules: {
+            "@limegrass/import-alias/import-alias": [
+                "error",
+                {
+                    relativeImportOverrides: [{ path: ".", depth: 0 }],
+                },
+            ],
+        },
     },
-    rules: {
-      "@limegrass/import-alias/import-alias": "error",
-    },
-  },
 ];
 
 export default eslintConfig;
